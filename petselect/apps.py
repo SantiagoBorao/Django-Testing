7f4fb8c421b1e7b3343826d03b3992d14a7bee79
SantiagoB <santiagoborao@gmail.com>
2021-11-20 13:38:48 +0100

    Created model and added to the path

--- petselect/apps.py
+++ petselect/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PetselectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'petselect'
+    name = 'petselect' #Nombre de la aplicación
--- petselect/models.py
+++ petselect/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=50) #Campo de carácteres de hasta solo 50 chars
+    hair = models.CharField(max_length=10) 
+    size = models.CharField(max_length=10)
